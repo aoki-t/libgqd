aebe95b82f0e64adae77d2bdd5fd50e58d4eb13a
aoki-t <code.aokit@gmail.com>
2016-01-28 22:21:35 +0900

    Add some methods and operators
    Constructor
    Destructor
    []operator
    =operator
    -()operater

--- libgqd/inc/gqd_basic.cu
+++ libgqd/inc/gqd_basic.cu
@@ -1,8 +1,168 @@
 #ifndef __GQD_BASIC_CU__
 #define __GQD_BASIC_CU__
 
+#include "gqd_real.h"
+#include "inline.cu"
 
-#include "common.cu"
+//#include "common.cu"
+
+//__device__ __constant__ double _qd_eps = (1.21543267145725e-63); // = 2^-209
+__device__ __constant__ double _qd_eps = (1.215432671457254239e-63); // = 2^-209
+
+
+__device__ __constant__ double __qd_zero = 0.0;
+__device__ __constant__ double __qd_one = 1.0;
+__device__ __constant__ double  __qd_inf[2];	// h_inf, CUDART_INF;
+__device__ __constant__ double __qd_qnan[2];	// h_qnan, CUDART_NAN;
+
+__device__ __constant__ double      __qd_e[4] = { 2.718281828459045091e+00, 1.445646891729250158e-16, -2.127717108038176765e-33, 1.515630159841218954e-49 };
+__device__ __constant__ double   __qd_log2[4] = { 6.931471805599452862e-01, 2.319046813846299558e-17, 5.707708438416212066e-34, -3.582432210601811423e-50 };
+__device__ __constant__ double  __qd_log10[4] = { 2.302585092994045901e+00, -2.170756223382249351e-16, -9.984262454465776570e-33, -4.023357454450206379e-49 };
+__device__ __constant__ double    __qd_2pi[4] = { 6.283185307179586232e+00, 2.449293598294706414e-16, -5.989539619436679332e-33, 2.224908441726730563e-49 };
+__device__ __constant__ double     __qd_pi[4] = { 3.141592653589793116e+00, 1.224646799147353207e-16, -2.994769809718339666e-33, 1.112454220863365282e-49 };
+__device__ __constant__ double    __qd_pi2[4] = { 1.570796326794896558e+00, 6.123233995736766036e-17, -1.497384904859169833e-33, 5.562271104316826408e-50 };
+__device__ __constant__ double __qd_pi1024[4] = { 3.067961575771282340e-03, 1.195944139792337116e-19, -2.924579892303066080e-36, 1.086381075061880158e-52 };
+__device__ __constant__ double    __qd_pi4[4] = { 7.853981633974482790e-01, 3.061616997868383018e-17, -7.486924524295849165e-34, 2.781135552158413204e-50 };
+__device__ __constant__ double   __qd_3pi4[4] = { 2.356194490192344837e+00, 9.1848509936051484375e-17, 3.9168984647504003225e-33, -2.5867981632704860386e-49 };
+
+
+// Constructors ================================================================
+// default constructor
+__device__ __host__
+gqd_real::gqd_real()
+: qd({ 0.0, 0.0, 0.0, 0.0 }) {
+}
+
+// destructor
+__device__ __host__
+gqd_real::~gqd_real(){
+}
+
+__device__ __host__ 
+gqd_real::gqd_real(double x0, double x1, double x2, double x3) {
+	qd.x = x0;
+	qd.y = x1;
+	qd.z = x2;
+	qd.w = x3;
+}
+
+__device__ __host__
+gqd_real::gqd_real(double d) {
+	qd.x = d;
+	qd.y = qd.z = qd.w = 0.0;
+}
+
+__device__ __host__
+gqd_real::gqd_real(const double *d4) {
+	qd.x = d4[0];
+	qd.y = d4[1];
+	qd.z = d4[2];
+	qd.w = d4[3];
+}
+
+
+__device__ __host__
+gqd_real::gqd_real(const gqd_real &a) {
+	qd.x = a.qd.x;
+	qd.y = a.qd.y;
+	qd.z = a.qd.z;
+	qd.w = a.qd.w;
+
+}
+
+__device__ __host__
+gqd_real::gqd_real(const gdd_real &a) {
+	qd.x = a.dd.x;
+	qd.y = a.dd.y;
+	qd.z = qd.w = 0.0;
+}
+
+__device__ __host__
+gqd_real::gqd_real(int i) {
+	qd.x = static_cast<double>(i);
+	qd.y = qd.z = qd.w = 0.0;
+}
+
+// Accessors ===================================================================
+__device__ __host__
+double gqd_real::operator[](int i) const {
+	assert(i < 4);
+
+	i = i % 4;
+	switch (i) {
+	case 0:
+		return qd.x;
+	case 1:
+		return qd.y;
+	case 2:
+		return qd.z;
+	case 3:
+		return qd.w;
+	default:
+#ifdef __CUDACC__
+		return __qd_qnan[1];
+#else
+		return std::numeric_limits<double>::quiet_NaN();
+#endif
+	}
+}
+
+__device__ __host__
+double &gqd_real::operator[](int i) {
+	assert(i < 4);
+
+	switch (i) {
+	case 0:
+		return qd.x;
+	case 1:
+		return qd.y;
+	case 2:
+		return qd.z;
+	case 3:
+		return qd.w;
+	default:
+		return qd.x;
+	}
+}
+
+// Assignments =================================================================
+/* quad-double = double */
+__device__ __host__
+gqd_real &gqd_real::operator=(double a) {
+	qd.x = a;
+	qd.y = qd.z = qd.w = 0.0;
+	return *this;
+}
+
+/* quad-double = double-double */
+__device__ __host__
+gqd_real &gqd_real::operator=(const gdd_real &a) {
+	qd.x = a.dd.x;
+	qd.y = a.dd.y;
+	qd.z = qd.w = 0.0;
+	return *this;
+}
+
+/* quad-double = quad-double */
+__device__ __host__
+gqd_real &gqd_real::operator=(const gqd_real &a) {
+	qd.x = a[0];
+	qd.y = a[1];
+	qd.z = a[2];
+	qd.w = a[3];
+	return *this;
+}
+
+__device__ __host__
+gqd_real gqd_real::operator-() const {
+	return gqd_real(-qd.x, -qd.y, -qd.z, -qd.w);
+}
+
+__device__ __host__
+gqd_real negative(const gqd_real &a) {
+	return gqd_real(-a.qd.x, -a.qd.y, -a.qd.z, -a.qd.w);
+}
+
 
 
 /** normalization functions */
